d93fad8305be8c1359093c75169e66c53400dc46
cristianames92 <cristian.ames@wolox.com.ar>
2017-02-05 20:19:58 -0300

    Initial comming plus basic tab navigation

---
+++ shopping-tracker/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  shopping-tracker
+//
+//  Created by Cristian Ames on 2/5/17.
+//  Copyright © 2017 Cristian Ames. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
